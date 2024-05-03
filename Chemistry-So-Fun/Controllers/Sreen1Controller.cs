0498407235f9082452c008ec7c59631aa8aa89f0
MinhPhambk <phamngocminh1230@gmail.com>
2024-05-03 19:40:35 +0700

    Init project

---
+++ Chemistry-So-Fun/Controllers/Sreen1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chemistry_So_Fun.Controllers
+{
+    public class Sreen1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Chemistry-So-Fun/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chemistry_So_Fun.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
