d4b7a1a3b0cf52afb01213d36fe07354eb6bc07b
MinhPhambk <phamngocminh1230@gmail.com>
2024-05-03 19:40:35 +0700

    Init project

---
+++ Chemistry-So-Fun/Controllers/Screen9Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chemistry_So_Fun.Controllers
+{
+    public class Screen9Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
